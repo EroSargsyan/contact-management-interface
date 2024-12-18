5997a47eb53c1d4e15c873937d5c40f178a56d2b
Erik Sargsyan <erosargsyan7@gmail.com>
2024-12-18 21:25:10 +0400

    feat(tailwind): configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import AppRouter from './routes/routes';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-50 text-gray-800">
       <AppRouter />
     </div>
   );
