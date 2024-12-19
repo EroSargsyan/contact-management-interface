4a6947866f93a7a0627dc817636fd161a3667820
Erik Sargsyan <erosargsyan7@gmail.com>
2024-12-19 17:59:46 +0400

    feat(routes): add routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import Home from '../pages/Home';
+import Layout from '../layouts/Layout';
+import CreateContactPage from '../pages/CreateContactPage';
+import ContactDetailsPage from '../pages/ContactDetailsPage';
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/create-contact" element={<CreateContactPage />} />
+          <Route path="/contacts/:id" element={<ContactDetailsPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
