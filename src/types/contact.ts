520bc65e1af5cf7929996ad254dc27b98192eec3
Erik Sargsyan <erosargsyan7@gmail.com>
2024-12-19 17:52:52 +0400

    feat(types): add iContact

--- src/types/contact.ts
+++ src/types/contact.ts
@@ -1,7 +1,7 @@
-export interface Contact {
+export interface IContact {
   id: number;
   name: string;
   username: string;
-  description: string;
-  profilePicture: string;
+  description?: string;
+  profilePicture?: string;
 }
