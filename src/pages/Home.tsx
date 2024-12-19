f08bf81ef79fc4201a838c01f12617422ae5c4d2
Erik Sargsyan <erosargsyan7@gmail.com>
2024-12-19 17:59:17 +0400

    feat(pages): add Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,7 @@
-import { useState } from 'react';
-import Sidebar from '../components/Sidebar/Sidebar';
-import ContactDetails from '../components/ContactDetails/ContactDetails';
-
 const Home: React.FC = () => {
-  const [selectedContactId, setSelectedContactId] = useState<number | null>(
-    null,
-  );
-
   return (
-    <div className="flex min-h-screen">
-      <Sidebar
-        selectedContactId={selectedContactId}
-        onSelectContact={setSelectedContactId}
-      />
-
-      <main className="flex-1 p-8 bg-gray-100">
-        <ContactDetails selectedContactId={selectedContactId} />
-      </main>
+    <div className="flex h-screen w-screen bg-gray-100">
+      <p>Select one of the contacts</p>
     </div>
   );
 };
