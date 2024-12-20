de3015e1d6f2ba3e398bf77177be7c3db9926348
Erik Sargsyan <erosargsyan7@gmail.com>
2024-12-20 17:34:21 +0400

    feat(Layout): add new styles

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,13 +3,15 @@ import Sidebar from '../components/Sidebar/Sidebar';
 
 const Layout: React.FC = () => {
   return (
-    <div className="flex h-screen w-screen bg-gray-100">
-      <aside className="w-1/4 bg-white shadow-lg h-full">
+    <div className="flex h-screen w-screen bg-gradient-to-r from-gray-50 to-gray-100">
+      <aside className="w-1/4 bg-white shadow-lg h-full border-r border-gray-200">
         <Sidebar />
       </aside>
 
-      <main className="flex-1 bg-white p-6 flex items-center justify-center">
-        <Outlet />
+      <main className="flex-1 bg-white p-8 overflow-auto">
+        <div className="max-w-5xl mx-auto">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
