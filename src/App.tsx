e0f84824da6d487703ccf639fdad55d40b1dffc9
Erik Sargsyan <erosargsyan7@gmail.com>
2024-12-20 22:20:03 +0400

    feat(App): add QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ContactsProvider } from './hooks/ContactsContext';
 import AppRouter from './routes/routes';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-800">
       <ContactsProvider>
-        <AppRouter />
+        <QueryClientProvider client={queryClient}>
+          <AppRouter />
+        </QueryClientProvider>
       </ContactsProvider>
     </div>
   );
